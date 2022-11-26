4471477307f9bba72053704f0809d7d903a8dd7c
gzim07 <bllikugezim@gmail.com>
2022-11-26 19:56:21 +0100

    created controllers func

--- database/databaseConnection.go
+++ database/databaseConnection.go
@@ -1,1 +1,36 @@
 package database
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+func DBinstance() *mongo.Client {
+	MongoDB := "mongodb+srv://gezim:0798@cluster0.wsfibbk.mongodb.net"
+	fmt.Print(MongoDB)
+	client, err := mongo.NewClient(options.Client().ApplyURI(MongoDB))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("connected to mongodb")
+	return client
+}
+
+var Client *mongo.Client = DBinstance()
+
+func OpenCollection(c *mongo.Client, collectname string) *mongo.Collection {
+
+	var collection *mongo.Collection = c.Database("restaurant").Collection(collectname)
+	return collection
+}
--- main.go
+++ main.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gzim07/restaurant-managment-backend/middleware"
 	"github.com/gzim07/restaurant-managment-backend/routes"
 )
 
 func main() {
+
 	router := gin.New()
 	router.Use(gin.Logger())
-
 	routes.UserRoute(router)
-	router.Use(middleware.Authentication())
+
 	routes.FoodRouter(router)
 	routes.InvoiceRouter(router)
 	routes.MenuRouter(router)
-
 	routes.OrderItemRouter(router)
 	routes.OrderRouter(router)
 	routes.TableRouter(router)
